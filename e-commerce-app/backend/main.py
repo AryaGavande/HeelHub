f9c064bf51143952af589c34802c4d48dd93e36a
Rital Patel <rspatell@unc.edu>
2024-02-24 21:06:45 -0500

    recent changes
    Co-authored-by: Bonnie Zhang <bonniez@unc.edu>
    Co-authored-by: Shreeya Sethi <shreeyasethi1@users.noreply.github.com>
    Co-authored-by: Arya <AryaGavande@users.noreply.github.com>

--- e-commerce-app/backend/main.py
+++ e-commerce-app/backend/main.py
@@ -1,7 +1,46 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+import sqlite3
+import uuid
+
+conn = sqlite3.connect('heelhub.db')
+
+cursor = conn.cursor()
+
+conn.row_factory = sqlite3.Row
+
+app = FastAPI()
 
 
 @app.get("/")
-
-def read_root():
+async def read_root():
     return {"Hello", "World"}
+
+@app.post("/product")
+async def write_product(service_type: str, amount: float, name: str):
+
+    # Write code here to write data we get from endpoint into database
+
+    product_id = uuid.uuid4().hex
+
+    cursor.execute(f'''
+        INSERT INTO products (product_id, service_type, amount, name) VALUES ('{product_id}', '{service_type}', {amount}, '{name}')
+    ''')
+
+    conn.commit()
+
+    return {
+        'product_id':product_id
+    }
+
+    return product_id
+
+
+@app.get("/product/{product_id}")
+async def read_product(product_id):
+    rows = cursor.execute(f"SELECT * FROM products where product_id = '{product_id}'").fetchone()
+    return {
+        'product_id': rows[0],
+        'name': rows[1],
+        'amount': rows[2],
+        'service_type':rows[3]
+    }
